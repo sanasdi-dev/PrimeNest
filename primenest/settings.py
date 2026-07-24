80f71e82b785f16e2bae880961203f7449552685
Sana Sdi <sana.saeidimoghadam@gmail.com>
2026-07-24 03:09:56 -0700

    Remove cloudinary config

--- primenest/settings.py
+++ primenest/settings.py
@@ -133,8 +133,4 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-)
+
